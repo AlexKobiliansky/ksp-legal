52c56080a9f4946f791d5aa8f5259c9e0581896b
alexey kobilianskiy <alex_kobilianskiy@bigmir.net>
2018-12-05 07:55:07 +0200

    consult section complete

--- app/js/common.js
+++ app/js/common.js
@@ -7,7 +7,22 @@ $(document).ready(function() {
     });
 
 
-    $(".num-item-desc").height('auto').equalHeights();
+    function heightses() {
+        if ($(window).width()>=481) {
+          /**/
+        }
+
+        $(".num-item-desc").height('auto').equalHeights();
+        $(".consult-col").height('auto').equalHeights();
+    }
+
+    $(window).resize(function() {
+        heightses();
+    });
+
+    heightses();
+
+
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
